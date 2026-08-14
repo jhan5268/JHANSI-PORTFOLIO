35901fbdbdea16953bf3713cf5ea0794b2ecb7fc
Jhansi Bonthu <jhansibonthu5268@gmail.com>
2026-08-14 21:38:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
 
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+
+const hiddenElements = document.querySelectorAll(".reveal");
+
+hiddenElements.forEach((element) => {
+    observer.observe(element);
+});
